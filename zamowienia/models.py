766a0697bd9ae07e794c4626103d5aefdee90cb0
MateuszBa <mateusz.babul@gmail.com>
2021-01-20 08:36:56 +0100

    Updated "Order" model

--- zamowienia/models.py
+++ zamowienia/models.py
@@ -7,18 +7,22 @@ class OrderId(models.Model):
     def __str__(self):
         return str(self.OrderId)
 
+class Client(models.Model):
+    Name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Name
+
 class Order(models.Model):
     Order_Id = models.ForeignKey(OrderId, on_delete=models.CASCADE)
     Item_Name = models.CharField(max_length=200)
     Catalog_Id = models.IntegerField(default=0)
-    Client_Name = models.CharField(max_length=200)
+    Client_Name = models.ForeignKey(Client, on_delete=models.CASCADE())
     Item_Cost = models.IntegerField(default=0)
 
     def __str__(self):
         return self.Item_Name
 
-class Client(models.Model):
-    Name = models.CharField(max_length=200)
-    Surname = models.CharField(max_length=200)
+
 
 
