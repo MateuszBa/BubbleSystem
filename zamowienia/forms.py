96bd91ba71b24966cd856e951785166ed1181522
MateuszBa <mateusz.babul@gmail.com>
2021-01-20 12:57:42 +0100

    Added "Add item" function to app

--- zamowienia/forms.py
+++ zamowienia/forms.py
@@ -1,11 +1,8 @@
 from django import forms
-from zamowienia.models import Order
+from zamowienia.models import Order, OrderId
 
 class NameForm(forms.ModelForm):
-    Item_Name = forms.CharField(label='Item Name', max_length=100)
-    Catalog_Id = forms.IntegerField(label='Catalog_Id')
-    Client_Id = forms.IntegerField(label='Client_Id')
 
     class Meta:
         model = Order
-        fields = ('Item_Name', 'Catalog_Id', 'Client_Id',)
+        fields = ('Order_Id','Item_Name', 'Catalog_Id','Item_Cost' ,'Client_Name')
--- zamowienia/views.py
+++ zamowienia/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import NameForm
 from django.db.models import Sum
+from django.urls import reverse
 
 from .models import Order, OrderId
 
@@ -20,18 +21,24 @@ def addorder(request):
     return render(request, 'zamowienia/addorder.html')
 
 def additem(request, Order_Id):
+    items = Order.objects.filter(Order_Id__OrderId=Order_Id)
+    suma = Order.objects.values('Order_Id').filter(Order_Id__OrderId=Order_Id).annotate(Sum('Item_Cost'))
+    var1 = OrderId.objects.filter(OrderId = Order_Id)
     if request.method == 'POST':
         form = NameForm(request.POST)
         if form.is_valid():
-            Order_Id__OrderId = Order_Id
             post = form.save(commit=False)
-            post.Order_Id = Order_Id__OrderId
+            post.Order_Id = var1[0]
             form.save()
-            return render(request, 'zamowienia/additem.html', {'form': form})
+            return render(request, 'zamowienia/detail.html', {'items': items, 'suma': suma[0]})
     else:
         form = NameForm()
 
     return render(request, 'zamowienia/additem.html', {'form': form})
+
+def usun(request):
+    #item = Order.objects.filter(Order_Id__OrderId=Order_Id)
+    return render(request, 'zamowienia/delete.html')
 
 def Clients(request):
     List_of_CLients = Order.objects.values('Client_Name__Name').annotate(Sum('Client_Name'))
